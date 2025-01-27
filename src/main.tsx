1a26d63117b6bc82edf49ae000b99c4fb105eaae
Dilum <dilumchandupa@gmail.com>
2025-01-27 20:06:27 +0530

    feat: Reorganize project structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "../App.tsx";
+import App from "./App.tsx";
 import { Provider } from "react-redux";
-import { store } from "../store/store.tsx";
+import { store } from "./store/store.tsx";
 import "./index.css";
 import { createTheme, ThemeProvider } from "@mui/material";
 
